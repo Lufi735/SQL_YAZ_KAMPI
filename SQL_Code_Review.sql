be29adde3d87426298350f84a4eadca8e645a752
Lufi <iu8264446@gmail.com>
2025-09-10 23:53:14 +0300

    TR Commit

--- SQL_Code_Review.sql
+++ SQL_Code_Review.sql
@@ -9,37 +9,37 @@ published_year INT CHECK(published_year BETWEEN 1900 AND 2025) ,/* CHECK(2025 >=
 added_at DATE
 );
 
-/* Insert Into  Tablo_adi(Attribute1, Attribute2...) Values(Deðer1, Deðer2...),(...),(...);*/
+/* Insert Into  Tablo_adi(Attribute1, Attribute2...) Values(Deger1, Deger2...),(...),(...);*/
 
 INSERT INTO books(title, author, genre, price, stock, published_year, added_at) 
-VALUES('Kayýp Zamanýn Ýzinde','M. Proust','Roman',129.90,25,1913,'2025-08-20'), 
-('Simyacý','Paulo Coelho','Roman',89.50,40,1988,'2025-08-21'),
+VALUES('KayÄ±p ZamanÄ±n Ä°zinde','M. Proust','Roman',129.90,25,1913,'2025-08-20'), 
+('SimyacÄ±','Paulo Coelho','Roman',89.50,40,1988,'2025-08-21'),
 ('Sapiens','Harari','Tarih',159.90,18,2011,'2025-08-25'),
-('Ýnce Memed','Y. Kemal','Roman',99.90,12,1955,'2025-08-22'),
-('Körlük','J. Saramago','Roman',119.00,7,1995,'2025-08-28'),
+('Ä°nce Memed','Y. Kemal','Roman',99.90,12,1955,'2025-08-22'),
+('KÃ¶rlÃ¼k','J. Saramago','Roman',119.00,7,1995,'2025-08-28'),
 ('Dune','F. Herbert','Bilim',149.00,30,1965,'2025-09-01'),
-('Hayvan Çiftliði','G. Orwell','Roman',79.90,55,1945,'2025-08-23'),
+('Hayvan Ã‡iftliÄŸi','G. Orwell','Roman',79.90,55,1945,'2025-08-23'),
 ('1984','G. Orwell','Roman',99.00,35,1949,'2025-08-24'),
-('Nutuk','M. K. Atatürk','Tarih',139,20,1927,'2025-08-27'),
-('Küçük Prens','A. de Saint-Exupéry','Çocuk',69.90,80,1943,'2025-08-26'),
-('Baþlangýç','D. Brown','Roman',109.00,22,2017,'2025-09-02'),
-('Atomik Alýþkanlýklar','J. Clear','Kiþisel Geliþim',129.00,28,2018,'2025-09-03'),
-('Zamanýn Kýsa Tarihi','S. Hawking','Bilim',119.50,16,1988,'2025-08-29'),
-('Þeker Portakalý','J. M. de Vasconcelos','Roman',84.90,45,1968,'2025-08-30'),
-('Bir Ýdam Mahkûmunun Son Günü','V. Hugo','Roman',74.90,26,1829,'2025-08-31') --1900 ile 2025 arasý olduðundan kütüphaneye giremedi
+('Nutuk','M. K. AtatÃ¼rk','Tarih',139,20,1927,'2025-08-27'),
+('KÃ¼Ã§Ã¼k Prens','A. de Saint-Exupery','Ã‡ocuk',69.90,80,1943,'2025-08-26'),
+('BaÅŸlangÄ±Ã§','D. Brown','Roman',109.00,22,2017,'2025-09-02'),
+('Atomik AlÄ±ÅŸkanlÄ±klar','J. Clear','KiÅŸisel GeliÅŸim',129.00,28,2018,'2025-09-03'),
+('ZamanÄ±n KÄ±sa Tarihi','S. Hawking','Bilim',119.50,16,1988,'2025-08-29'),
+('Åžeker PortakalÄ±','J. M. de Vasconcelos','Roman',84.90,45,1968,'2025-08-30'),
+('Bir Ä°dam Mahkumunun Son GÃ¼nÃ¼','V. Hugo','Roman',74.90,26,1829,'2025-08-31') --1900 ile 2025 arasi oldugundan kutuphaneye giremedi
 
 --TUM KITAPLAR :
-SELECT * FROM books ORDER BY book_id ASC; -- !!!! Hocam, birden fazla yürüttüðüm için kitaplar çoðalmýþtý bende tabloyu silmiþtim soldan ve yeniden yürütmüþtüm. 
--- Fakat id'ler resetlenmemiþ sanýrým sebebi nedir?
+SELECT * FROM books ORDER BY book_id ASC; -- !!!! Hocam, birden fazla yuruttugum icin kitaplar cogalmisti bende tabloyu silmistim soldan ve yeniden yurutmustum. 
+-- Fakat id'ler resetlenmemis sanirim sebebi nedir?
 
 --SORGU 1 :
-SELECT title, price, author FROM books ORDER BY price ASC; -- ASC = Ascending (artan sýra) , DESC = Descending (azalan sýra)
+SELECT title, price, author FROM books ORDER BY price ASC; -- ASC = Ascending (artan sira) , DESC = Descending (azalan sira)
 
 --SORGU 2 :
 SELECT * FROM books WHERE genre ='Roman' ORDER BY title ASC;
-/* - SQL'de büyük küçük harf duyarlýlýðý yok (ROMAN yazarsan da sorun olmaz)
- - Eðerki N'roman' yazarsan, alfabe formatý deðiþir.(eðer roman yerine öykü yazacaksan hata vermesin diye.) (mesela ASCII -> UTF-8). N = National (yerli harfler için)
- - '*' iþareti tüm attribute larý otomatik seçmek için kullanýlýr. */
+/* - SQL'de buyuk kucuk harf duyarliligi yok (ROMAN yazarsan da sorun olmaz)
+ - Egerki N'roman' yazarsan, alfabe formati degisir.(eger roman yerine oyku yazacaksan hata vermesin diye.) (mesela ASCII -> UTF-8). N = National (yerli harfler icin)
+ - '*' isareti tum attribute lari otomatik secmek icin kullanilir. */
 
 --SORGU 3 :
 SELECT * FROM books WHERE price BETWEEN 80 AND 120;
@@ -48,23 +48,23 @@ SELECT * FROM books WHERE price BETWEEN 80 AND 120;
 SELECT title, stock FROM books WHERE stock < 20;
 
 --SORGU 5 :
-SELECT * FROM books WHERE title LIKE '%zaman%';/* LIKE = zaman kelimesini içerenler (% = karakter var.(0 veya daha fazla)(Büyük ve küçük harfe duyarlý deðil.)) */
+SELECT * FROM books WHERE title LIKE '%zaman%';/* LIKE = zaman kelimesini icerenler (% = karakter var.(0 veya daha fazla)(Buyuk ve kucuk harfe duyarli degil.)) */
 
 --SORGU 6 :
-SELECT * FROM books WHERE genre IN ('Bilim','Roman');/* IN = Genre'sý parantez içindeki deðerlerden biri olanlarý seç. */
+SELECT * FROM books WHERE genre IN ('Bilim','Roman');/* IN = Genre'si parantez icindeki degerlerden biri olanlari sec. */
 
 --SORGU 7 :
 SELECT * FROM books WHERE published_year > 2000 ORDER BY published_year DESC;
 
 --SORGU 8 :
 SELECT * FROM books WHERE added_at >= DATEADD(DAY,-10,GETDATE()); 
--- GETDATE() = güncel tarihi ve zamaný alýr , DATEADD(type,amount,date) = istenen miktar ve cinsteki zamaný verilen tarihe ekler
+-- GETDATE() = guncel tarihi ve zamani alir , DATEADD(type,amount,date) = istenen miktar ve cinsteki zamani verilen tarihe ekler
 
 --SORGU 9 :
 SELECT TOP 5 * FROM books ORDER BY price DESC; -- LIMIT = MySQL, PoStgreSQL (SELECT * FROM ... LIMIT 5 ...) ; TOP = SQL Server (SELECT TOP 5 * FROM ....)
--- LIMIT çalýþmadý hocam. O yüzden TOP kullandým.
+-- LIMIT calismadi hocam. O yuzden TOP kullandim.
 
 --SORGU 10 :
 SELECT * FROM books WHERE stock BETWEEN 30 AND 60 ORDER BY price ASC;
 
--- Okuduðunuz için teþekkürler Hocam!
+-- Okudugunuz icin tesekkurler Hocam!
